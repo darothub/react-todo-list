b086e8770bf83d231b679700e8b71f8062406cdc
darothub <darotudeen@gmail.com>
2019-07-02 11:54:27 +0100

    add form

--- app.jsx
+++ app.jsx
@@ -4,10 +4,19 @@ class App extends React.Component{
             <div className="container">
                 <Header />
                 <Nav />
+                <Form />
             </div>
         )
        
     }
+}
+
+const Header =()=>{
+    return (
+        <div className="bg-light mt-2 text-center">
+            <h1 className="display-4"><span className="text-info ml-2">Todo List</span></h1>
+        </div>
+    )
 }
 
 const Nav =()=>{
@@ -21,11 +30,17 @@ const Nav =()=>{
     )
 }
 
-const Header =()=>{
+const Form =()=>{
     return (
-        <div className="bg-light mt-2 text-center">
-            <h1 className="display-4"><span className="text-info ml-2">Todo List</span></h1>
-        </div>
+        
+            <form>
+                <div className="form-group">
+                    <label htmlFor="input" className="lead text-light">Task</label>
+                    <input type="text" className="form-control"   placeholder="Enter task" />
+                    <button type="submit" className="btn btn-success btn-block mt-2">Submit</button>
+                </div>
+            </form>
+       
     )
 }
 ReactDOM.render(<App />, document.getElementById("root"));
